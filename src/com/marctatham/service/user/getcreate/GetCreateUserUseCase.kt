324954ff069a58118c878ed284c065ffb6d832aa
Marc Tatham <marctatham@gmail.com>
2020-09-24 21:19:47 +0100

    updated to use the logger instead of print

--- src/com/marctatham/service/user/getcreate/GetCreateUserUseCase.kt
+++ src/com/marctatham/service/user/getcreate/GetCreateUserUseCase.kt
@@ -3,9 +3,10 @@ package com.marctatham.service.user.getcreate
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserRecord
 import com.marctatham.SimpleJWT
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class GetCreateUserUseCase(
-
     // TODO: Retrieve user info from firebase
     // we should probably go down to the data layer here
     // both to verify the token, and to get the user data
@@ -14,17 +15,20 @@ class GetCreateUserUseCase(
 ) {
 
     suspend fun execute(getCreateUserSession: GetCreateUserSessionEntity): UserSessionEntity {
-
         // verify the token and get the
         val decodedToken = firebaseAuth.verifyIdToken(getCreateUserSession.idToken)
 
         // fetch user info from firebase
         val userId = decodedToken.uid
         val userRecord: UserRecord = firebaseAuth.getUser(userId)
-        println("user: [${userRecord.displayName}] - email: [${userRecord.email}]")
+        logger.debug("user: [${userRecord.displayName}] - email: [${userRecord.email}]")
 
-        // issue jwt token
+        // issue jwt token for this user
         val jwtToken = jwtIssuer.sign(userId)
         return UserSessionEntity(jwtToken)
+    }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(GetCreateUserUseCase::class.java)
     }
 }
